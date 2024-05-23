0acc69ade3fc2325497412c05ed8aacf63497bf4
Artprozew <artprozew@gmail.com>
2024-05-23 15:29:16 -0300

    Refactor: update code to reflect folder name change
    - Little code refactor
    - Change logger scope to work with logging.conf

--- src/data_handler/fetch_data.py
+++ src/data_handler/fetch_data.py
@@ -5,17 +5,21 @@ from typing import Dict
 
 import requests
 
-logger = logging.getLogger(__name__)
 
 def fetch_all_data() -> tuple[list[str], Dict[str, str]]:
+    logger = logging.getLogger(__name__)
     logger.info("Requesting data from API endpoint...")
 
-    json_file: os.PathLike = "data.json"
     data: list[str] = []
     world_data: Dict[str, str] = {}
-    request: requests.Response
 
-    path: os.PathLike = "assets/characters"
+    root_dir: os.PathLike = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    data_dir: os.PathLike = os.path.join(root_dir, "data")
+    characters_dir: os.PathLike = os.path.join(data_dir, "characters")
+    world_dir: os.PathLike = os.path.join(data_dir, "world")
+
+    json_file: os.PathLike = os.path.join(characters_dir, "characters_data.json")
+    request: requests.Response
     do_request: bool = True
 
     try:
@@ -23,14 +27,14 @@ def fetch_all_data() -> tuple[list[str], Dict[str, str]]:
     except requests.exceptions.RequestException:
         do_request = False
 
-    if not os.path.exists(path):
-        logger.info(f'Directory "{path}" does not exists. Creating one...')
-        os.makedirs(path)
+    if not os.path.exists(characters_dir):
+        logger.info(f'Directory "{characters_dir}" does not exists. Creating one...')
+        os.makedirs(characters_dir)
 
-    if not do_request:
+    if not do_request: # Backup that may be outdated
         logger.info("Couldn't request data from API, getting from already existing file")
-        if os.path.exists(os.path.join(path, json_file)):
-            with open(os.path.join(path, json_file), "r+") as file:
+        if os.path.exists(json_file):
+            with open(json_file, "r+") as file:
                 data = json.load(file)
         else:
             raise(RuntimeError("Couldn't get/request any data!"))
@@ -44,17 +48,17 @@ def fetch_all_data() -> tuple[list[str], Dict[str, str]]:
         data.remove("hu-tao")
         data.append("hutao")
 
-    with open(os.path.join(path, json_file), "w+") as file:
+    with open(json_file, "w+") as file: # May break with auto-py-to-exe?
         logger.info("Dumping data to JSON file") # Saving latest data to JSON file
         json.dump(data, file, ensure_ascii=False, indent=4)
 
 
-    path = "assets/world"
-    if os.path.exists(os.path.join(path, json_file)):
-        with open(os.path.join(path, json_file), "r+") as file:
+    json_file: os.PathLike = os.path.join(world_dir, "world_data.json")
+    if os.path.exists(json_file):
+        with open(json_file, "r+") as file:
             world_data = json.load(file)
     else:
-        raise(FileNotFoundError(f'File {os.path.join(path, json_file)} does not exists!'))
+        raise(FileNotFoundError(f'File {json_file} does not exists!'))
 
     logger.info("Requests complete")
     return data, world_data
