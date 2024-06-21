327ac74af1bf77aebdd18d39231007b982f48e77
Arthur Carvalho <artprozew@gmail.com>
2024-06-21 11:57:47 -0300

    refactor: add utils function to handle exceptions

---
+++ src/utils/exception_manager.py
@@ -0,0 +1,14 @@
+from typing import Any
+import tempfile
+import os
+import sys
+import traceback
+
+def exception_handler(exc_type: Any, exc_value: Any, tb: Any) -> None:
+    with open(f"{tempfile.gettempdir()}\\GenshinRichPresence\\traceback.txt", "w") as file:
+        for line in traceback.format_exception(exc_type, exc_value, tb):
+            file.writelines(line)
+
+    traceback.print_exception(exc_type, exc_value, tb)
+    os.system("pause")
+    sys.exit(-1)
