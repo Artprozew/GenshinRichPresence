dfab72d39e6713d04a3105c9c151bee995fc3b18
Arthur Carvalho <artprozew@gmail.com>
2024-06-26 23:05:46 -0300

    feat: allow RPC customization and use of variables

--- src/rich_presence.py
+++ src/rich_presence.py
@@ -1,11 +1,15 @@
-import config
-import time
-import pypresence
 import logging
-from utils import handle_exit
-from typing import Final, Optional
+import re
+import time
 from types import FrameType
+from typing import Any, Union
+
+import pypresence
+
+import config
+from config import configRPC_configs, configRPC_characters, configRPC_regions
 from game_monitor import GameMonitor
+from utils import handle_exit
 
 
 class DiscordRichPresence(pypresence.Presence):
@@ -15,13 +19,16 @@ class DiscordRichPresence(pypresence.Presence):
     details: str = "Unknown"
 
     large_image: str = "genshin"
-    small_image: str = "unknown"
+    small_image: str = "genshin"
     large_text: str = "Genshin Impact"
     small_text: str = "Unknown"
 
     previous_region: str = "Unknown"
     current_region: str = "Unknown"
     current_character: str = "Unknown"
+    current_weapon: str = "Unknown"
+
+    game_name: str = "genshin"
 
     last_update: float = time.time() - 10000
     updatable: bool = True
@@ -31,16 +38,52 @@ class DiscordRichPresence(pypresence.Presence):
         self._logger: logging.Logger = logging.getLogger(__name__)
 
         self.game_monitor: GameMonitor = game_monitor
+        self.connected = False
         handle_exit.handle_exit_hook(self._teardown, 0, None)
 
-    def _teardown(self, _signal_number: int, _stack_frame: Optional[FrameType]) -> None:
-        self._logger.warning("Clearing Rich Presence")
-        self.clear()
+        self.rpc_configs: dict[str, list[Any]] = {
+            "large_image": [configRPC_configs["large_image"], []],
+            "large_text": [configRPC_configs["large_text"], []],
+            "small_image": [configRPC_configs["small_image"], []],
+            "small_text": [configRPC_configs["small_text"], []],
+            "state": [configRPC_configs["state"], []],
+            "details": [configRPC_configs["details"], []],
+        }
+        self.parse_configs()
+
+    def parse_configs(self) -> None:
+        variables: dict[str, str] = {
+            "user_activity": "self.is_user_active",
+            "region": "current_region",
+            "character": "current_character",
+            "weapon": "current_weapon",
+            "game": "game_name",
+            "character_image": "character_image",
+            "region_image": "region_image",
+        }
+        compiled_regex: re.Pattern[str] = re.compile(r"%\((.*?)\)")
+
+        for name in configRPC_configs.keys():
+            matches: list[str] = re.findall(compiled_regex, configRPC_configs[name])
+
+            for match in matches:
+                if match not in variables.keys():
+                    raise ValueError(f"{name}: {match} is not a valid variable")
+
+                self.rpc_configs[name][0] = self.rpc_configs[name][0].replace(f"%({match})", "{}")
+                self.rpc_configs[name][1].append(variables[match])
+
+    def _teardown(self, _signal_number: int, _stack_frame: Union[FrameType, None]) -> None:
+        if self.connected:
+            self._logger.warning("Clearing Rich Presence")
+            self.clear()
+            self.connected = False
 
     def connect(self) -> None:
         self._logger.info("Connecting Rich Presence")
         self.update_event_loop(pypresence.utils.get_event_loop())
         self.loop.run_until_complete(self.handshake())
+        self.connected = True
 
     def can_update_rpc(self) -> bool:
         if (time.time() - self.last_update) > config.RPC_UPDATE_RATE:
@@ -51,11 +94,32 @@ class DiscordRichPresence(pypresence.Presence):
     def set_last_update(self) -> None:
         self.last_update = time.time()
 
+    def is_user_active(self) -> str:
+        return "in-game" if self.game_monitor.is_user_active() else "inactive"
+
+    def get_parsed_configs(self, name: str) -> Union[str, Any]:
+        all_attrib: list[str] = []
+
+        for attribute in self.rpc_configs[name][1]:
+            if attribute == "character_image":
+                if self.current_character in configRPC_characters:
+                    all_attrib.append(configRPC_characters[self.current_character])
+                else:
+                    all_attrib.append("".join(self.current_character.split()).lower())
+            elif attribute == "region_image":
+                if self.current_region in configRPC_regions:
+                    all_attrib.append(configRPC_regions[self.current_region])
+                else:
+                    all_attrib.append("None")
+            elif attribute == "self.is_user_active":
+                all_attrib.append(self.is_user_active())
+            else:
+                all_attrib.append(getattr(self, attribute))
+
+        return self.rpc_configs[name][0].format(*all_attrib)
+
     def update_rpc(self) -> None:
         self.game_monitor.check_changed_focus()
-
-        if not self.details or not self.current_region:
-            self.details = "On Menus"
 
         # Workaround: Fixes the incorrect displayed region as Liyue when the player is on The Chasm
         # Issue #27: https://github.com/Artprozew/GenshinRichPresence/issues/27
@@ -63,17 +127,14 @@ class DiscordRichPresence(pypresence.Presence):
             self._logger.debug("Setting region as the_chasm instead of liyue to workaround #27")
             self.current_region = "The Chasm"
 
-        is_user_active: Final[str] = "In-Game" if self.game_monitor.is_user_active() else "Inactive"
-        self.details = f"{is_user_active}. Exploring {self.current_region}"
-
         self.update(
             start=self.game_monitor.get_process_create_time(),
-            state=f"Playing as {self.current_character}",
-            details=self.details,
-            large_image="genshin",
-            small_image=self.small_image,
-            large_text="Genshin Impact",
-            small_text=self.current_character,
+            state=self.get_parsed_configs("state"),
+            details=self.get_parsed_configs("details"),
+            large_image=self.get_parsed_configs("large_image"),
+            small_image=self.get_parsed_configs("small_image"),
+            large_text=self.get_parsed_configs("large_text"),
+            small_text=self.get_parsed_configs("small_text"),
         )
 
         self.set_last_update()
