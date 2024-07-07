ee587334e81a09da5076045c8d767592e866a816
Arthur Carvalho <artprozew@gmail.com>
2024-07-07 18:45:56 -0300

    refactor: update exception_handler() behavior
    - add use of main directory constant (changes the traceback.txt location)
      this is done to ease the access to those files if needed
    - replace pause with time sleep as there's plan to hide the command
      line and show only an icon on the system tray, pause would keep it
      waiting forever

--- src/utils/exception_manager.py
+++ src/utils/exception_manager.py
@@ -8,10 +8,10 @@ from utils.handle_exit import safe_exit
 
 
 def exception_handler(exc_type: Any, exc_value: Any, tb: Any) -> None:
-    with open(f"{tempfile.gettempdir()}\\GenshinRichPresence\\traceback.txt", "w") as file:
+    with open(os.path.join(config.MAIN_DIRECTORY, "traceback.txt"), "w") as file:
         for line in traceback.format_exception(exc_type, exc_value, tb):
             file.writelines(line)
 
     traceback.print_exception(exc_type, exc_value, tb)
-    os.system("pause")
-    sys.exit(-1)
+    time.sleep(5)
+    safe_exit()
